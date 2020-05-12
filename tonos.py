4699e562fb98efbeb355459ee025e72de89a8c45
Matthias Dörfelt <hi@mokafolio.de>
2020-05-12 13:50:54 -0700

    ...

--- tonos.py
+++ tonos.py
@@ -6,18 +6,18 @@ hostname = "tonos"
 soundcloud_key = None
 
 #install pip
-subprocess.call("sudo apt install python3-pip", shell=True)
+# subprocess.call("sudo apt install python3-pip", shell=True)
 
 #install mopidy
 #https://docs.mopidy.com/en/latest/installation/debian/
-subprocess.call("wget -q -O - https://apt.mopidy.com/mopidy.gpg | sudo apt-key add -", shell=True)
-subprocess.call("sudo wget -q -O /etc/apt/sources.list.d/mopidy.list https://apt.mopidy.com/buster.list", shell=True)
-subprocess.call("sudo apt update", shell=True)
-subprocess.call("sudo apt install mopidy", shell=True)
+# subprocess.call("wget -q -O - https://apt.mopidy.com/mopidy.gpg | sudo apt-key add -", shell=True)
+# subprocess.call("sudo wget -q -O /etc/apt/sources.list.d/mopidy.list https://apt.mopidy.com/buster.list", shell=True)
+# subprocess.call("sudo apt update", shell=True)
+# subprocess.call("sudo apt install mopidy", shell=True)
 
-#install extensions
-subprocess.call("sudo apt install mopidy-mpd", shell=True)
-subprocess.call("sudo apt install mopidy-soundcloud", shell=True)
+# #install extensions
+# subprocess.call("sudo apt install mopidy-mpd", shell=True)
+# subprocess.call("sudo apt install mopidy-soundcloud", shell=True)
 
 #adjust mopidy config
 
